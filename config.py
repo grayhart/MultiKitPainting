d043f284e25210c9959b098543a767646d7bcb25
Graham Hart <graham@the-harts.co.uk>
2021-12-04 19:00:58 +0000

    Split the groups by source kit Allow the choice of kit name or kit id to identify kits Allow ancestor choice Split into the correct side and group Add notes Probably a lot of other things !

--- config.py
+++ config.py
@@ -6,5 +6,25 @@ Created on Fri Dec  3 15:18:30 2021
 """
 
 SIBLING_LIST = ['Alan J D Hart', 'Brian F Hart', 'Kenneth Leslie HART']
+
+# Set use names to True to use names rather than Ids for the match lists. 
+# If you use names, then a check will be made that there is only one of that name and processing will 
+# stop otherwise
+USE_NAMES=True
+
+# Example with USE_NAME=False
+#BOTH_LIST = ['D-F13C8F58-A50F-498B-8844-A4D74EA40E74', 'D-8FE030CD-9545-4220-B1C6-FA0D853F9B0D', 'D-CC3A5967-8A51-4B85-B7E6-2C24D1F780F6', 
+#              'D-FC5DE884-8932-4C50-A07A-7F97CCB7CB0A', 'D-D7958A02-69A3-470D-A066-9FCA0D077D6C' ]
+#MATERNAL_LIST = ['D-5CC0AFE3-1301-4CEF-9C76-F457F6F34EA1']
+#PATERNAL_LIST = ['D-34A434EA-EE7A-4930-BAAE-1BC3BB8E7FF9']
+#ANCESTOR_LIST = ['D-34A434EA-EE7A-4930-BAAE-1BC3BB8E7FF9','D-5CC0AFE3-1301-4CEF-9C76-F457F6F34EA1']
+
+# Example with USE_NAME=True
+BOTH_LIST = ['Lesley Ann', 'Graham Michael Hart', 'Rosemary Ann Hart', 'Patrick Kenneth Kehoe', 'Roisin Frances Kehoe']
+MATERNAL_LIST = ['Beryl M Martell']
+PATERNAL_LIST = ['Lyn Steele']
+ANCESTOR_LIST = ['Lyn Steele','Beryl M Martell']
+
+
 INPUT_FOLDER = 'InputData'
 OUTPUT_FOLDER = 'OutputFolder'
--- mkp.py
+++ mkp.py
@@ -18,10 +18,15 @@ Created on Fri Dec  3 15:05:29 2021
 #
 # Give a list of siblings and I will open and read their files.
 #
+#
+# DNAPainter input needs to be:
+# Chr Start End cM SNPs Match Confidence Group Side Notes Colour
+    
 import config
 import os
 import glob
 import pandas as pd
+import numpy as np
 
 
 # Look for a single file for each sibling in the input folder and read it in
@@ -50,6 +55,16 @@ def read_files(sibling_list):
         unique_id = firstentry.split("-D-")[0]
         sibhash['uniqueid'] = unique_id
         sibhash['data'] = file_df
+        
+        # Add columns for the unique ids of the kit and the match
+        sibhash['data']['kit_id'] = unique_id
+        
+        def add_match_id(x):
+            return  "D-" + x.split("-D-")[1]    
+        
+        sibhash['data']['match_id'] = sibhash['data']['DNA Match ID'].apply(add_match_id)
+        sibhash['data']['orig_match_name'] = sibhash['data']['Match name']
+        sibhash['data']['kit_match_name'] = name
         df_array[name] = sibhash
     return df_array
 
@@ -95,12 +110,81 @@ def prepare_converted_matches(df_array):
         df_array[sibkey]['data'] = sibling_df
     return df_array
     
-def combine_and_save_output(df_array):
-    output_file = config.OUTPUT_FOLDER + "\\outputfile.csv"
+def combine_output(df_array):
     comb_df = pd.DataFrame()
     for sibkey in df_array:
         comb_df = comb_df.append(df_array[sibkey]['data'])
-    comb_df.to_csv(output_file)
+    return comb_df
+
+def format_and_add_group_and_side(df_array):
+    # Sort and name the fields into the order that DNA Painter would like 
+    # Chr Start End cM SNPs Match Confidence Group Side Notes Colour
+    mapper = { 
+        'Index' : 'Index',
+        'DNA Match ID' : 'DNA Match ID',
+        'Name' : 'Name',
+        'Match name' : 'Match',
+        'Chromosome' : 'Chr',
+        'Start Location' : 'Start',
+        'End Location' : 'End',
+        'Start RSID' : 'Start RSID',
+        'End RSID' : 'End RSID',
+        'Centimorgans' : 'cM',
+        'SNPs' : 'SNPs'
+        }
+    
+    # Add the unique IDs and other information into the Notes
+    def create_notes(x):
+        return 'Created by MultikitPainter. Unique kit-match-id for MyHeritage = ' + x
+        
+    df_array['Notes'] = df_array['DNA Match ID']
+    df_array['Notes'] = df_array['Notes'].apply(create_notes)
+    
+    df_array = df_array.rename(columns=mapper)
+    df_array['Confidence'] = 0
+    df_array['Group'] = ''
+    df_array['Side'] = ''
+    df_array['Colour'] = ''
+    df_array['Ancestor'] = ''
+    
+    # Set the various sides and groups
+    # df0['New Col1'] = np.where((df0['Col'] > 0), 'Increasing', 
+    #                      np.where((df0['Col'] < 0), 'Decreasing', 'No Change'))
+    
+    if config.USE_NAMES:
+        matchfield = 'orig_match_name'        
+    else:
+        matchfield = 'match_id'
+        
+    
+    
+    for match in config.SIBLING_LIST:
+        df_array['Group'] = np.where((df_array['orig_match_name'] == match),df_array['kit_match_name'] + ' - ' + df_array['orig_match_name'], df_array['Group'])
+        df_array['Side'] = np.where((df_array['orig_match_name'] == match),'Both', df_array['Side'])
+
+    for match in config.BOTH_LIST:
+        df_array['Group'] = np.where((df_array[matchfield] == match),df_array['kit_match_name'] + ' - ' + df_array['orig_match_name'], df_array['Group'])
+        df_array['Side'] = np.where((df_array[matchfield] == match),'Both', df_array['Side'])
+        
+    for match in config.PATERNAL_LIST:
+        df_array['Group'] = np.where((df_array[matchfield] == match),df_array['kit_match_name'] + ' - ' + df_array['orig_match_name'], df_array['Group'])
+        df_array['Side'] = np.where((df_array[matchfield] == match),'Paternal', df_array['Side'])
+        
+    for match in config.MATERNAL_LIST:
+        df_array['Group'] = np.where((df_array[matchfield] == match),df_array['kit_match_name'] + ' - ' + df_array['orig_match_name'], df_array['Group'])
+        df_array['Side'] = np.where((df_array[matchfield] == match),'Maternal', df_array['Side'])
+    
+    for match in config.ANCESTOR_LIST:
+        df_array['Ancestor'] = np.where((df_array[matchfield] == match), 1, df_array['Ancestor'])
+        
+        
+    return df_array
+    
+def save_output(df_array):
+    output_file = config.OUTPUT_FOLDER + "\\outputfile.csv"
+
+    
+    df_array.to_csv(output_file,index=False, columns=('Chr', 'Start', 'End', 'cM', 'SNPs', 'Match', 'Confidence', 'Group', 'Side', 'Notes', 'Colour', 'Ancestor'))
     
 
 # Read in the files 
@@ -113,7 +197,13 @@ df_array = remove_duplicate_sibling_entries(df_array)
 df_array = prepare_converted_matches(df_array)
 
 # Combine and save the output 
-combine_and_save_output(df_array)
+df_array = combine_output(df_array)
+
+# Add the group and side where known
+df_array = format_and_add_group_and_side(df_array)
+
+# Write the output
+save_output(df_array)
 
 
 
