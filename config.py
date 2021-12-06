3017cc1ad8e52a5649e65d29d3b6f670fac993af
Graham Hart <graham@the-harts.co.uk>
2021-12-06 15:25:54 +0000

    Update config.py
    Anonymising

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ Created on Fri Dec  3 15:18:30 2021
 @author: graham
 """
 
-SIBLING_LIST = ['Alan J D Hart', 'Brian F Hart', 'Kenneth Leslie HART']
+SIBLING_LIST = ['Alan Smith', 'Brian Smith', 'Kenneth Smith']
 
 # Set use names to True to use names rather than Ids for the match lists. 
 # If you use names, then a check will be made that there is only one of that name and processing will 
@@ -13,17 +13,17 @@ SIBLING_LIST = ['Alan J D Hart', 'Brian F Hart', 'Kenneth Leslie HART']
 USE_NAMES=True
 
 # Example with USE_NAME=False
-#BOTH_LIST = ['D-F13C8F58-A50F-498B-8844-A4D74EA40E74', 'D-8FE030CD-9545-4220-B1C6-FA0D853F9B0D', 'D-CC3A5967-8A51-4B85-B7E6-2C24D1F780F6', 
-#              'D-FC5DE884-8932-4C50-A07A-7F97CCB7CB0A', 'D-D7958A02-69A3-470D-A066-9FCA0D077D6C' ]
-#MATERNAL_LIST = ['D-5CC0AFE3-1301-4CEF-9C76-F457F6F34EA1']
-#PATERNAL_LIST = ['D-34A434EA-EE7A-4930-BAAE-1BC3BB8E7FF9']
-#ANCESTOR_LIST = ['D-34A434EA-EE7A-4930-BAAE-1BC3BB8E7FF9','D-5CC0AFE3-1301-4CEF-9C76-F457F6F34EA1']
+#BOTH_LIST = ['D-F13C8F58-A50F-498B-8844-A4D74EA434E5', 'D-8FE030CD-8763-4220-B1C6-FA0D853F9B0D', 'D-CC3A5967-3215-4B85-B7E6-2C24D1F780F6', 
+#              'D-FC5DE884-9893-4C50-A07A-7F97CCB7CB0A', 'D-D7958A02-9876-470D-A066-9FCA0D077D6C' ]
+#MATERNAL_LIST = ['D-5CC0AFE3-2342-4CEF-9C76-F457F6F34EA1']
+#PATERNAL_LIST = ['D-34A434EA-2312-4930-BAAE-1BC3BB8E7FF9']
+#ANCESTOR_LIST = ['D-34A434EA-3453-4930-BAAE-1BC3BB8E7FF9','D-5CC0AFE3-3211-4CEF-9C76-F457F6F34EA1']
 
 # Example with USE_NAME=True
-BOTH_LIST = ['Lesley Ann', 'Graham Michael Hart', 'Rosemary Ann Hart', 'Patrick Kenneth Kehoe', 'Roisin Frances Kehoe']
-MATERNAL_LIST = ['Beryl M Martell']
-PATERNAL_LIST = ['Lyn Steele']
-ANCESTOR_LIST = ['Lyn Steele','Beryl M Martell']
+BOTH_LIST = ['Lesley Ann', 'Graham Jones', 'Marvin Hart']
+MATERNAL_LIST = ['Beryl French']
+PATERNAL_LIST = ['Lyn Bottle']
+ANCESTOR_LIST = ['Lyn Bottle','Beryl French']
 
 
 INPUT_FOLDER = 'InputData'
